4d93c89c346f2e0690c928075753658b1b0a4fe5
Zak Habul <zakhabul@gmail.com>
2023-05-22 16:23:15 -0700

    adding rotation movement, and creating lists in GameManager and AI controller

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour // asbtract means it cannot be instaitated
+{
+    public virtual void Start()
+    {
+
+    }
+
+    public virtual void Update()
+    {
+
+    }
+}
+
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -13,8 +13,21 @@ public class PlayerController : Controller
     // Start is called before the first frame update
     public override void Start() //overides Controller Start
     {
-        playerPawn = GetComponent<MachinePawn>(); // <- tells the function what it returns in the angle brackets ("<>") , so it looks at every 
-        base.Start();                             // component on the same game object it's on. 
+        playerPawn = GetComponent<MachinePawn>(); // <- tells the function what it returns in the angle brackets ("<>") , so it looks at every component on the same game object it's on. 
+        
+        if (GameManager.Instance) //Does the gamemanger exist
+        {
+            GameManager.Instance.players.Add(this); //if so, add PlayerController to the List, which then transfers to "players" list
+        }
+        base.Start();                         
+    }
+
+    private void OnDestroy() //When you stop simulating, everything technically gets destroyed, don't put crazy stuff in OnDestroy. 
+    {
+        if (GameManager.Instance) 
+        {
+            GameManager.Instance.players.Remove(this); //removes PlayerController from the List. 
+        }
     }
 
     // Update is called once per frame
